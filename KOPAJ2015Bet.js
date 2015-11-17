9ad86060eb6f8298e0ddb47064588af1c9d73625
KG <kerezsigergo@hotmail.com>
2015-11-17 23:59:40 +0100

    database listing

---
+++ KOPAJ2015Bet.js
@@ -0,0 +1,14 @@
+PlayerList = new Mongo.Collection('players');
+
+
+if (Meteor.isClient) {
+
+
+
+	Template.leaderboard.helpers({
+	    'player': function(){
+	        return PlayerList.find()
+	    }
+	});
+
+}
