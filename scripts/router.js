17f101dde8a2b8d037a60597f4791754bcec3edb
Péter Nyíri <Pter Nyri>
2015-11-18 01:14:26 +0100

    no message

--- scripts/router.js
+++ scripts/router.js
@@ -10,6 +10,14 @@ Router.route('/home', {
   layoutTemplate: 'layout',
 
 });
+Router.route('/adminSzint', {
+  layoutTemplate: 'adminSzint',
+
+});
+Router.route('/adminEvent', {
+  layoutTemplate: 'adminEvent',
+
+});
 /**
  * Router definition for the chat room page.
  * Subscribes to the required publications.
